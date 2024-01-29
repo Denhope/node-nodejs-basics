064c77b0709f808842e26f2984c9252a8e58490e
Denis Kavalchuk <den_hope@mail.ru>
2024-01-29 23:06:13 +0300

    feat:done Zlib (src/zip) tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,36 @@
+import zlib from "zlib";
+import fs from "fs";
+import path from "path";
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = "./src/zip/files";
+  const archivePath = path.join(__dirname, "archive.gz");
+  const outputPath = path.join(__dirname, "fileToCompress.txt");
+
+  return new Promise((resolve, reject) => {
+    const gunzip = zlib.createGunzip();
+    const source = fs.createReadStream(archivePath);
+    const destination = fs.createWriteStream(outputPath);
+
+    source.on("error", (err) => {
+      reject(err);
+    });
+
+    gunzip.on("error", (err) => {
+      reject(err);
+    });
+
+    destination.on("error", (err) => {
+      reject(err);
+    });
+
+    destination.on("finish", () => {
+      console.log("File has been decompressed successfully.");
+      resolve();
+    });
+
+    source.pipe(gunzip).pipe(destination);
+  });
 };
 
 await decompress();
